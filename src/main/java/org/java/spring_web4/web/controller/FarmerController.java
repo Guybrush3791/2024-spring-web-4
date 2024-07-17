4bca6ef3ea59cec504d1be5d8c61f5d2393a2737
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-17 15:34:57 +0200

    update

--- src/main/java/org/java/spring_web4/web/controller/FarmerController.java
+++ src/main/java/org/java/spring_web4/web/controller/FarmerController.java
@@ -10,6 +10,7 @@ import org.java.spring_web4.db.serv.FarmerService;
 import org.java.spring_web4.web.dto.FarmerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/farmer")
+@CrossOrigin(origins = "http://localhost:5173")
 public class FarmerController {
 
     @Autowired
